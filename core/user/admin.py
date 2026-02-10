1324f21b119e51814f9c869a5173b743801f6e07
adrian <adrianarrechea@yahoo.com.ar>
2026-02-10 16:59:47 -0300

    Feat: Add username to User admin configuration

--- core/user/admin.py
+++ core/user/admin.py
@@ -4,10 +4,11 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
     ordering = ('email',)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('username', 'email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'photo')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
